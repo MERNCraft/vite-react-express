41eecf252ec0a26f5682f6e8003ad1eb7f76fc01
James Newton <blackslate@lexogram.com>
2024-12-01 09:16:28 +0100

    Fix errors

--- serveCookie.js
+++ serveCookie.js
@@ -7,21 +7,16 @@ const { getToken } = require('./jwToken')
 
 const serveCookie = (req, res, next) => {
   const { path } = req
-  // console.log("serveCookie path:", path)
   
   if ( path === "/" || path === "index.html" ) {
-     // The request is for the static index.html page at the origin
-     const protocol = req.protocol
-     const host = req.headers.host // includes the port
-    //  const regex = `^https?:\/\/${host}/`
-     const origin = `${protocol}://${host}/`
-     console.log("origin:", origin)
+    // The request is for the static index.html page at the origin
+    const host = req.headers.host // includes the port
+    const regex = `^https?:\/\/${host}/`
 
-    //  console.log("regex:", regex)
+    console.log("regex:", regex)
     
     // Create a token to record the origin that was requested...
-    // const pass = getToken(regex)
-    const pass = getToken(origin)
+    const pass = getToken(regex)
     // ... and serve it as session cookie.
     req.session.pass = pass
 
